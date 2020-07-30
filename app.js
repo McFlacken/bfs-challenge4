344acbec4cd064853309c407d4648e76e90ba71b
McFlacken <angel.i.martinez@gmail.com>
2020-07-29 20:18:56 -0600

    fix GET route to serve random image from db

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express 			= require("express"),
 	mongoose 			= require("mongoose");
 
 //APP CONFIG
-mongoose.connect("mongodb+srv://mackie:%238u1LtfR0m%24@cluster0.sz6gu.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://mackie:%238u1LtfR0m%24@cluster0.sz6gu.mongodb.net/uglycats?retryWrites=true&w=majority", {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true
@@ -18,15 +18,44 @@ mongoose.connect("mongodb+srv://mackie:%238u1LtfR0m%24@cluster0.sz6gu.mongodb.ne
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+
+var catSchema = new mongoose.Schema({
+	url: String,
+	value: Number
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
+// Cat.create({
+// 	url: "https://cdn2.thecatapi.com/images/ahk.jpg",
+// 	value: 0
+// 	}, function(err, cat){
+// 		if(err){
+// 			console.log("something is not right");
+// 			console.log(err);
+// 		} else {
+// 			console.log("you added a new cat");
+// 			console.log(cat);
+// 		}
+// });
+
+
 app.get("/", function(req,res){
 	res.redirect("cats");
 });
 
-app.get("/cats", function(req,res){
-	
-	res.render("cats");
-});
+app.get("/cats", function(req, res){
 
+	Cat.countDocuments().exec(function(err, count){
+		var random = Math.floor(Math.random() * count);
+		Cat.findOne().skip(random).exec(
+			function (err, result) {
+		// result is random 
+			res.render("cats", {randomCat: result})
+	});
+  });
+	
+});
 
 app.listen(process.env.PORT || 3000, function(){
 	console.log('In cloud far away... The Cat App has now started...');
