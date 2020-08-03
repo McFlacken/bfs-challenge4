c4a87f57ad058eceb4103994e3578d532d0a5ecc
McFlacken <angel.i.martinez@gmail.com>
2020-08-02 20:30:28 -0600

    still working on the PATCH routes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express 			= require("express"),
+	methodOverride		= require("method-override"),
 	app 				= express(),
 	bodyParser 			= require("body-parser"),
+	axios				= require("axios"),
 	mongoose 			= require("mongoose");
 
-//APP CONFIG
+//CONNECT TO THE DATABASE
 mongoose.connect("mongodb+srv://mackie:%238u1LtfR0m%24@cluster0.sz6gu.mongodb.net/uglycats?retryWrites=true&w=majority", {
 	useNewUrlParser: true,
 	useCreateIndex: true,
@@ -16,23 +18,18 @@ mongoose.connect("mongodb+srv://mackie:%238u1LtfR0m%24@cluster0.sz6gu.mongodb.ne
 
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //this allows me to use EJS templates, I find it easier
+app.use(methodOverride("_method")); //this allows me to use PATCH even when sending a POST request
 
-
+// CAT SCHEMA, just a url and a value because the id will be given automatically when saved to the db
 var catSchema = new mongoose.Schema({
 	url: String,
 	value: Number
 });
 
-var voteSchema = new mongoose.Schema({
-	value: Number
-	
-});
-
 var Cat = mongoose.model("Cat", catSchema);
 
-var Vote = mongoose.model("Vote", voteSchema);
-
+//FUNCTION TO CREATE A CAT, TO SEED THE DATABASE
 // Cat.create({
 // 	url: "https://cdn2.thecatapi.com/images/ahk.jpg",
 // 	value: 0
@@ -46,7 +43,8 @@ var Vote = mongoose.model("Vote", voteSchema);
 // 		}
 // });
 
-//GET ROUTES - default and /cats
+
+//GET ROUTES - default and /cats, could be the same one, but I think /cats is nicer
 
 app.get("/", function(req,res){
 	res.redirect("cats");
@@ -62,18 +60,22 @@ app.get("/cats", function(req, res){
   });
 });
 
-//GET ROUTE - CHECK OUT THE VOTES
 
+//GET ROUTE - CHECK OUT THE VOTES - in progress......
 app.get("/votes", function(req, res){
 	res.render("votes");
 })
 
-//POST ROUTE - ADD VOTES
+//PATCH ROUTE - ADD VOTES - this is where I'm stuck...
 
-// app.post("/addvote", function(req, res){
+app.patch("/cats/ugly", function(req, res){
+	res.send("you are trying to vote for ugly")
+})
 
-// 	res.redirect("votes")
-// })
+app.patch("/cats/uglier", function(req, res){
+	res.send("you are trying to vote for uglier")
+})
+
 
 
 app.listen(process.env.PORT || 3000, function(){
